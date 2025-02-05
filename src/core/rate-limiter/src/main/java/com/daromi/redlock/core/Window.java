ecd5edb12917452572d480b85e4ac2c8128dd918
daromi94 <davromero07@gmail.com>
2025-02-04 20:18:03 -0500

    refactor: :recycle: convert window to record

--- src/core/rate-limiter/src/main/java/com/daromi/redlock/core/Window.java
+++ src/core/rate-limiter/src/main/java/com/daromi/redlock/core/Window.java
@@ -1,19 +1,8 @@
 package com.daromi.redlock.core;
 
-import java.util.Objects;
+public record Window(long from, long to) {
 
-public final class Window {
-
-  private final long from;
-
-  private final long to;
-
-  private Window(final long from, final long to) {
-    this.from = from;
-    this.to = to;
-  }
-
-  public static Window of(final long from, final long to) {
+  public Window {
     if (from < 0 || to < 0) {
       throw new IllegalArgumentException("window bounds must be non-negative");
     }
@@ -21,33 +10,5 @@ public final class Window {
     if (from >= to) {
       throw new IllegalArgumentException("'from' must be strictly less than 'to'");
     }
-
-    return new Window(from, to);
-  }
-
-  public long getFrom() {
-    return from;
-  }
-
-  public long getTo() {
-    return to;
-  }
-
-  @Override
-  public boolean equals(final Object other) {
-    if (this == other) {
-      return true;
-    }
-
-    if (!(other instanceof Window window)) {
-      return false;
-    }
-
-    return this.from == window.from && this.to == window.to;
-  }
-
-  @Override
-  public int hashCode() {
-    return Objects.hash(from, to);
   }
 }
